5dacf889792728839ecda7b8e1f0f2e1dd241d2c
andy-j92 <andyjang0530@gmail.com>
2017-01-25 00:23:36 +1300

    adding project

---
+++ Assets/Scripts/SkyboxAnimator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxAnimator : MonoBehaviour {
+
+    int step = 1;
+
+    // Update is called once per frame
+    void Update () {
+        var skybox = RenderSettings.skybox;
+
+        step += 1;
+        step %= 360;
+
+        skybox.SetInt("Rotation", step);
+        RenderSettings.skybox = skybox;
+    }
+}
